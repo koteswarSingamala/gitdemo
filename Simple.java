27657020590a5ca3b11392c8913d45f173185e12
AravindThallapaka <tallaphakaaravind@gmail.com>
2020-08-07 11:34:48 +0530

    2

--- Simple.java
+++ Simple.java
@@ -6,5 +6,8 @@ public static void main(String args[])
 System.out.println("Hello Java");
 System.out.println("Hello Java");
 System.out.println("Hello Java");
+System.out.println("Hello Java");
+System.out.println("Hello Java");
+System.out.println("Hello Java");
 }
 }
